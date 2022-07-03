3fe890fd1a7abc08e411a1b9a46d7de0328caa85
Laurynas Vaitiekunas <laurynasvait@gmail.com>
2022-07-03 14:11:20 +0300

    Redux Implemented

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createLogger } from "redux-logger";
+import ThunkMiddleware from "redux-thunk";
 import "./index.css";
 import App from "./containers/App";
 import reportWebVitals from "./reportWebVitals";
+import { searchRobots, requestRobots } from "./reducers";
 import "tachyons";
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots });
+const store = createStore(
+  rootReducer,
+  applyMiddleware(ThunkMiddleware, logger)
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
